2e064be2e2f0eaa54401854b5fa93537c0f104fd
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2021-02-14 16:55:54 +0500

    First Commit

---
+++ Java Script/js.js
@@ -0,0 +1,94 @@
+// JavaScript Document
+function myfunction()
+{
+	document.getElementById("whisk").style.border = "thick solid #00F";
+}
+function myfunction1()
+{
+	document.getElementById("veg").style.border = "thick solid #00F";
+}
+function myfunction2()
+{
+	document.getElementById("set").style.border = "thick solid #00F";
+}
+function myfunction3()
+{
+	document.getElementById("spoon").style.border = "thick solid #00F";
+}
+function myfunction4()
+{
+	document.getElementById("rolling").style.border = "thick solid #00F";
+}
+function myfunction5()
+{
+	document.getElementById("pot").style.border = "thick solid #00F";
+}
+function myfunction6()
+{
+	document.getElementById("nut").style.border = "thick solid #00F";
+}
+function myfunction7()
+{
+	document.getElementById("Can").style.border = "thick solid #00F";
+}
+function myfunction8()
+{
+	document.getElementById("peelers").style.border = "thick solid #00F";
+}
+function myfunction9()
+{
+	document.getElementById("tongs").style.border = "thick solid #00F";
+}
+function myfunction10()
+{
+	document.getElementById("Cutters").style.border = "thick solid #00F";
+}
+function myfunction11()
+{
+	document.getElementById("Basting").style.border = "thick solid #00F";
+}
+function myfunction12()
+{
+	document.getElementById("scissors").style.border = "thick solid #00F";
+}
+function myfunction13()
+{
+	document.getElementById("herb").style.border = "thick solid #00F";
+}
+function myfunction14()
+{
+	document.getElementById("cups").style.border = "thick solid #00F";
+}
+function myfunction15()
+{
+	document.getElementById("Colanders").style.border = "thick solid #00F";
+}
+function myfunction16()
+{
+	document.getElementById("spoon").style.border = "thick solid #00F";
+}
+function myfunction15()
+{
+	document.getElementById("spoon").style.border = "thick solid #00F";
+}
+function myfunction16()
+{
+	document.getElementById("pestle").style.border = "thick solid #00F";
+}
+function myfunction17()
+{
+	document.getElementById("Juicers").style.border = "thick solid #00F";
+}
+function myfunction18()
+{
+	document.getElementById("ladles").style.border = "thick solid #00F";
+}
+function myfunction19()
+{
+	document.getElementById("Timers").style.border = "thick solid #00F";
+}
+function myfunction20()
+{
+	document.getElementById("spatulas").style.border = "thick solid #00F";
+}
+
